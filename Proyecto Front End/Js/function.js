e67797f9508f92329fd36e330f14898ce2862ebf
EdgarWainz <125988055+EdgarWainz@users.noreply.github.com>
2023-02-27 18:09:50 -0300

    Add files via upload

---
+++ Proyecto Front End/Js/function.js
@@ -0,0 +1,15 @@
+document.getElementById('boton_verperfil').addEventListener('clic', function () {
+    document.body.style.display = "none";
+});
+
+document.getElementById('boton_ocultarperfil').addEventListener('clic', function () {
+    document.body.style.display = "";
+});
+
+document.getElementById('boton_dark').addEventListener('clic', function () {
+    document.body.style.backgroundColor = '#000000';
+});
+
+document.getElementById('boton_blanco').addEventListener('clic', function () {
+    document.body.style.backgroundColor = "#FFFFFF";
+});
